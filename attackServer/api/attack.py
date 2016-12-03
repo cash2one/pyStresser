598b0220673d8a40cdbb5b167f90920841cbdc22
obby <obnoxious@dongcorp.org>
2016-12-03 12:53:18 -0800

    validation stuffz

--- attackServer/api/attack.py
+++ attackServer/api/attack.py
@@ -7,20 +7,33 @@ app = Flask(__name__)
 
 @app.route("/attack/<api_key>/<method>/<target>/<length>")
 def attack(api_key, method, target, length):
-	access_keys = json.loads(open('keys.json', 'r').read())
 	
-	if api_key in access_keys['keys']:
+	access_keys = json.loads(open('keys.json', 'r').read())
+
+	if api_key in access_keys['keys'][0]:
 		pass
 	else:
 		return json.dumps({'success': False, 'message': 'Sorry, your key is invalid! Fuck off hacker scum.'})
 	
-	if re.match(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', target):
+	if re.match(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[0-65535]{1,5}$', target):
 		pass
 	else:
 		return json.dumps({'success': False, 'message': 'Not a valid target. Please reconsider your life. :)'})
 
-	#attack = handler.attacks[method]()
-	#return attack.attack()
+	if int(length) <= int(access_keys['keys'][0][api_key]):
+		pass
+	else:
+		return json.dumps({'success': False, 'message': 'Length is either invalid or exceeds time permitted.'})
+
+	try:
+		attack = handler.attacks[method]()
+	except:
+		return json.dumps({'success': False, 'message': 'Attack method does not exist, the fuck you think this is?'})
+
+	try:
+		return attack.attack(target, method, length)
+	except:
+		return json.dumps({'success': False, 'message': 'Attack failed early, unsure why.'})
 
 if __name__ == "__main__":
     app.run()
--- attackServer/api/handler.py
+++ attackServer/api/handler.py
@@ -1,14 +1,16 @@
 import sys
 import os
+import json
 
 class udp():
 	def __init__(self):
-		self.attack()
+		return
 
-	def attack(self):
-		
-
-		return { 'success': False }
+	def attack(self, target, method, time):
+		try:
+			return json.dumps({'success': True, 'message': 'Attack launched on %s using %s for %s seconds.'%(target, method, time)})
+		except:
+			return json.dumps({'success': False, 'message': 'Launching attack failed, unsure why.'})
 
 attacks = {
 	'udp': udp,
